cdcbf363a536951ab483b6c9ba74c6b51c9b4909
Valdes Che <valdesche03@gmail.com>
2019-12-11 21:11:28 +0100

    Created the session factory class

---
+++ src/main/java/utils/HibernateUtils.java
@@ -0,0 +1,43 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ * Created by ValdoR on 2019-12-11.
+ */
+
+public class HibernateUtils {
+
+    private static final SessionFactory sessionFactory;
+    public static final String HIBERNATE_SESSION = "HIBERNATE_SESSION";
+
+    static{
+
+        try {
+
+            System.out.println("Attempting to configure a session factory (SF) !");
+
+            Configuration configuration = new Configuration().configure();
+
+            ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().
+                    applySettings(configuration.getProperties()).buildServiceRegistry();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            System.out.println("Session Factory created correctly !");
+        } catch (Exception ex) {
+            System.out.println("An error occurred during the Session Factory creation : " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
