6c0a8b5aab7ceeb44aa362a5509ba731baa096a2
Valdes Che <valdesche03@gmail.com>
2019-12-12 09:56:12 +0100

    Added Abstract controller

---
+++ src/main/java/com/alphahotel/controller/AbstractController.java
@@ -0,0 +1,32 @@
+package com.alphahotel.controller;
+
+import com.alphahotel.utils.FacesContextUtil;
+import com.alphahotel.utils.JSFMessageUtil;
+import com.sun.xml.internal.ws.client.RequestContext;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by ValdoR on 2019-12-12.
+ */
+public class AbstractController {
+    private static final String KEEP_DIALOG_OPENED = "KEEP_DIALOG_OPENED";
+
+    public AbstractController() {
+        super();
+    }
+
+    protected void displayErrorMessage(String message){
+        JSFMessageUtil jsfmu = new JSFMessageUtil();
+        jsfmu.sendErrorMessage(message);
+    }
+
+    protected void displayInfoMessage(String message){
+        JSFMessageUtil jsfmu = new JSFMessageUtil();
+        jsfmu.sendInfoMessage(message);
+    }
+
+
+
+}
