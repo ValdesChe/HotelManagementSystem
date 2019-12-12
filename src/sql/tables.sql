7056dcce87b6d13f352e4a97c5c000a5c19494ed
Valdes Che <valdesche03@gmail.com>
2019-12-12 12:46:52 +0100

    Fixed #4 by removing maven build configuration for sql seed

--- src/sql/tables.sql
+++ src/sql/tables.sql
@@ -1,17 +1,19 @@
-CREATE TABLE alphahotel.utilisateur
+CREATE TABLE IF NOT EXISTS alphahotel.utilisateur
 (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   nom VARCHAR(30),
   prenom VARCHAR(30),
   telephone VARCHAR(30),
-  login VARCHAR(35),
-  email VARCHAR(35) NULL,
+  login VARCHAR(35) UNIQUE,
+  email VARCHAR(35) NULL UNIQUE,
   password_hash VARCHAR(150),
   created_at DATE ,
   updated_at DATE,
   status VARCHAR(20),
   role VARCHAR(20)
 );
-CREATE UNIQUE INDEX utilisateur_id_uindex ON alphahotel.utilisateur (id);
+/*
+CREATE UNIQUE INDEX  utilisateur_id_uindex ON alphahotel.utilisateur (id);
 CREATE UNIQUE INDEX utilisateur_login_uindex ON alphahotel.utilisateur (login);
 CREATE UNIQUE INDEX utilisateur_email_uindex ON alphahotel.utilisateur (email);
+*/
