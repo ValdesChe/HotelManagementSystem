453019f966d72f9bdaf3697b63ab62d054e12b12
Valdes Che <valdesche03@gmail.com>
2019-12-16 20:42:31 +0100

    Set administrator filter for security

---
+++ src/main/java/com/alphahotel/filters/AbstractFilter.java
@@ -0,0 +1,28 @@
+package com.alphahotel.filters;
+
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+/**
+ * Created by ValdoR on 2019-12-16.
+ */
+public class AbstractFilter {
+
+    public AbstractFilter() {
+        super();
+    }
+
+    protected void doLogin(ServletRequest request, ServletResponse response, HttpServletRequest req) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("login.xhtml");
+        dispatcher.forward(request, response);
+    }
+
+    protected void accessDenied(ServletRequest request, ServletResponse response, HttpServletRequest req) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("access-denied.xhtml");
+        dispatcher.forward(request, response);
+    }
+}
