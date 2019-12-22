4bf4a9d5051ef08c2a88b07f2a0a89ee34515f2a
Valdes Che <valdesche03@gmail.com>
2019-12-22 20:48:04 +0100

    Added Mailer util class for sending email

---
+++ src/main/java/com/alphahotel/utils/Mailer.java
@@ -0,0 +1,45 @@
+package com.alphahotel.utils;
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.Serializable;
+import java.util.Properties;
+/**
+ * Created by ValdoR on 2019-12-22.
+ */
+public class Mailer implements Serializable{
+    static Properties props = new Properties();
+    public Mailer(){
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+    }
+
+    public static void send(String to,String sub,String msg){
+        //get Session
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("fromTemporaryEmail@","yourTempSecuredPass");
+                    }
+                });
+        //compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO ,new InternetAddress(to));
+            message.setSubject(sub);
+            message.setText(msg);
+            //send message
+            Transport.send(message);
+            System.out.println("message sent successfully");
+        }  catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
